f0fb52c7a92121052ea903c84e768c1b7e3db37c
Vitaliy <ivahiv99@gmail.com>
2024-04-01 14:22:14 +0300

    refactor: replaced mongoose with mongodb library

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const app = express();
 
+const connectToDB = async () => {
+    const connectionUrl = 'mongodb+srv://learningNode:coolpass@test-cluster.tsx3pds.mongodb.net/?retryWrites=true&w=majority&appName=test-cluster';
+    const client = new MongoClient(connectionUrl);
+
+
+    await client.connect();
+    console.log('Connected to mongo client');
+
+    const dbName = 'todo-app';
+    const db = client.db(dbName);
+    const collection = db.collection('todo-tickets');
+}
+
+connectToDB().then(() => app.listen(3000)).catch(err => console.log(err));
+
 app.get('/all-tickets', (req, res) => {
+
+// make a request to mongodb here
     const ticketList = [
         {
             text: 'GET tickets',
@@ -16,7 +34,7 @@ app.get('/all-tickets', (req, res) => {
             isDone: false,
         },
     ];
+
+
     res.json(ticketList);
 });
-
-app.listen(3000);
