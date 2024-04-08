d7b24fe18bf9279ce68d5f6705fca936cd7e18da
Vitaliy <ivahiv99@gmail.com>
2024-04-08 15:30:35 +0300

    feat: endpoint for ticket updates, work on delete-ticket endpoint

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { ObjectId } = require('mongodb');
 const { connectToDB, getCollection } = require('./mongodbConnection');
 const app = express();
+app.use(express.json())
 
 connectToDB((err) => {
     if(!err) {
@@ -20,4 +22,46 @@ app.get('/all-tickets', async (req, res) => {
     });
 
     res.json(ticketList);
+});
+
+app.get('/ticket/:id', async (req, res) => {
+
+});
+
+app.post('/add-ticket', async (req, res) => {
+
+});
+
+app.patch('/edit-ticket/:id', async (req, res) => {
+    const id = req.params.id;
+    const update = req.body;
+
+    const collection = getCollection();
+    if (ObjectId.isValid(id)) {
+        try {
+            const afterInsert = await collection.updateOne({_id: new ObjectId(id)}, {$set: update});
+            res.json(afterInsert);
+        } catch(error) {
+            res.status(500).json({err: error});
+        }
+    } else {
+        res.status(500).json({err: 'incorrect ticket id'});
+    }
+
+});
+
+app.delete('/remove-ticket/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const collection = getCollection();
+    if (ObjectId.isValid(id)) {
+        try {
+            const afterDelete = await collection.deleteOne({_id: new ObjectId(id)});
+            res.json(afterDelete);
+        } catch(error) {
+            res.status(500).json({err: error});
+        }
+    } else {
+        res.status(500).json({err: 'incorrect ticket id'});
+    }
 });
